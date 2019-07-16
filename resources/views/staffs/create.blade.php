060bd773392e07b59d1e27e9c1a98b62af6f9359
Lephuong96 <bigankroi@gmail.com>
2019-07-16 12:13:20 +0700

    update create-edit

--- resources/views/staffs/create.blade.php
+++ resources/views/staffs/create.blade.php
@@ -1,56 +1,57 @@
 @extends('home')
 @section('title', 'Thêm mới nhân viên')
 @section('content')
+    <div class="text-primary text-md-center">
+        <h2>Thêm mới nhân viên</h2>
+    </div>
     <div class="row">
-        <div class="col-md-12">
-            <h2>Thêm mới nhân viên</h2>
-        </div>
+
         <div class="col-md-12">
             <form method="post" action="{{ route('staffs.store') }}">
                 @csrf
                 <div class="row">
                     <div class="col">
-                        <label>Mã nhân viên</label>
+                        <label>Mã nhân viên:</label>
                         <input type="text" class="form-control" name="id" required>
                     </div>
                     <div class="col">
-                        <label>Số điện thoại</label>
+                        <label>Số điện thoại:</label>
                         <input type="integer" class="form-control" name="phone_number" required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <label>Nhóm nhân viên</label>
+                        <label>Nhóm nhân viên:</label>
                         <input type="text" class="form-control" name="group_employees" required>
                     </div>
                     <div class="col">
-                        <label>Số CMND</label>
+                        <label>Số CMND:</label>
                         <input type="integer" class="form-control" name="card_number" required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <label>Họ tên</label>
+                        <label>Họ tên:</label>
                         <input type="text" class="form-control" name="name" required>
                     </div>
                     <div class="col">
-                        <label>Email</label>
+                        <label>Email:</label>
                         <input type="email" class="form-control" name="email" required>
                     </div>
                 </div>
                 <div class="row">
                     <div class="col">
-                        <label>Ngày sinh</label>
+                        <label>Ngày sinh:</label>
                         <input type="date" class="form-control" name="birthday" required>
                     </div>
 
                     <div class="col">
-                        <label>Địa chỉ</label>
+                        <label>Địa chỉ:</label>
                         <input type="text" class="form-control" name="address" required>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label>Giới tính</label>
+                    <label>Giới tính:</label>
                     <input name="gender" type="radio" value="Nam"/>Nam
                     <input name="gender" type="radio" value="Nữ"/>Nữ
                 </div>
--- resources/views/staffs/edit.blade.php
+++ resources/views/staffs/edit.blade.php
@@ -2,48 +2,59 @@
 @section('title', 'Chỉnh sửa nhân viên')
 
 @section('content')
+    <div class="text-primary text-md-center">
+        <h2>Chỉnh sửa nhân viên</h2>
+    </div>
     <div class="row">
-        <div class="col-md-12">
-            <h2>Chỉnh sửa nhân viên</h2>
-        </div>
+
         <div class="col-md-12">
             <form method="post" action="{{ route('staffs.update', $staff->id) }}" >
                 @csrf
-                <div class="form-group">
-                    <label >Mã nhân viên</label>
-                    <input type="text" class="form-control" name="id" required value="{{$staff->id}}">
+                <div class="row">
+                    <div class="col">
+                        <label>Mã nhân viên:</label>
+                        <input type="text" class="form-control" name="id" required value="{{$staff->id}}">
+                    </div>
+                    <div class="col">
+                        <label>Số điện thoại:</label>
+                        <input type="integer" class="form-control" name="phone_number" required value="{{$staff->phone_number}}">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label>Nhóm nhân viên:</label>
+                        <input type="text" class="form-control" name="group_employees" required value="{{$staff->group_employees}}">
+                    </div>
+                    <div class="col">
+                        <label>Số CMND:</label>
+                        <input type="integer" class="form-control" name="card_number" required value="{{$staff->card_number}}">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label>Họ tên:</label>
+                        <input type="text" class="form-control" name="name" required value="{{$staff->name}}">
+                    </div>
+                    <div class="col">
+                        <label>Email:</label>
+                        <input type="email" class="form-control" name="email" required value="{{$staff->email}}">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <label>Ngày sinh:</label>
+                        <input type="date" class="form-control" name="birthday" required value="{{$staff->birthday}}">
+                    </div>
+
+                    <div class="col">
+                        <label>Địa chỉ:</label>
+                        <input type="text" class="form-control" name="address" required value="{{$staff->address}}">
+                    </div>
                 </div>
                 <div class="form-group">
-                    <label >Nhóm nhân viên</label>
-                    <input type="text" class="form-control"  name="group_employees" required value="{{$staff->group_employees}}">
-                </div>
-                <div class="form-group">
-                    <label >Họ tên</label>
-                    <input type="text"  class="form-control" name="name" required value="{{$staff->name}}">
-                </div>
-                <div class="form-group">
-                    <label >Ngày sinh</label>
-                    <input type="date"  class="form-control" name="birthday" required value="{{$staff->birthday}}">
-                </div>
-                <div class="form-group">
-                    <label >Giới tính</label>
-                    <input type="text"  class="form-control" name="gender" required value="{{$staff->gender}}">
-                </div>
-                <div class="form-group">
-                    <label >Số điện thoại</label>
-                    <input type="integer"  class="form-control" name="phone_number" required value="{{$staff->phone_number}}">
-                </div>
-                <div class="form-group">
-                    <label >Số CMND</label>
-                    <input type="integer"  class="form-control" name="card_number" required value="{{$staff->card_number}}">
-                </div>
-                <div class="form-group">
-                    <label >Email</label>
-                    <input type="email"  class="form-control" name="email" required value="{{$staff->email}}">
-                </div>
-                <div class="form-group">
-                    <label >Địa chỉ</label>
-                    <input type="text"  class="form-control" name="address" required value="{{$staff->address}}">
+                    <label>Giới tính:</label>
+                    <input name="gender" type="radio" value="Nam"/>Nam
+                    <input name="gender" type="radio" value="Nữ"/>Nữ
                 </div>
                 <button type="submit" class="btn btn-primary">Cập nhật</button>
                 <a href="{{route('staffs.index')}}" class="btn btn-dark">Hủy</a>
